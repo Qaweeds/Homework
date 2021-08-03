c9584146d15966563bfc3a47e1e97c0f706b0af3
Qaweeds <qaweeds@gmail.com>
2021-08-03 11:05:06 +0300

    move include to the top

--- 01-08/vendor/Config.php
+++ 01-08/vendor/Config.php
@@ -1,11 +1,10 @@
 <?php
-
+include_once './_autoload.php';
 
 class Config
 {
     public static function getConfig()
     {
-        include_once './_autoload.php';
         $config = BASEDIR . 'config.ini';
         if (file_exists($config)) {
             return parse_ini_file($config);
